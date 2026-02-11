49172a1534aaf6f7e0b6d1e3d37d6ca0e1da6b46
Haimon Vieira <haimonvieira@pm.me>
2026-02-11 20:56:45 -0300

    att

--- DSM2/dw2-nodejs-express/js-aula01-tipos-de-funcoes/variaveis/script.js
+++ DSM2/dw2-nodejs-express/js-aula01-tipos-de-funcoes/variaveis/script.js
@@ -5,6 +5,31 @@
 //let: utilizado quando for necessario reatribuir o valor da variavel
 //const: utilizar quando nao precisar reatribuir o valor da variavel
 
+//Deixar criar a variavel com o mesmo nome
 var cidade = "Registro";
 var cidade = "Sete Barras";
+
 document.writeln(cidade);
+document.writeln("<br>");
+//let nao deixar criar nova variavel com mesmo nome
+let nome = "Haimon";
+//mas pode reatribuir o conteudo
+nome = "Caroline";
+
+document.writeln(nome);
+document.writeln("<br>");
+
+
+//Como e uma constante ele nem imprime na tela, com erro de 
+//atribuir a um tipo constante de variavel
+const pais = "Brasil";
+pais = "Argentina"
+
+document.writeln(pais);
+document.writeln("<br>");
+
+
+
+
+
+

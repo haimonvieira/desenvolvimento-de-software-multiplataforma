10b36f20ff93e5b75d7cf248b9071602ecab7f3f
Haimon Vieira <haimonvieira@pm.me>
2026-02-11 20:48:00 -0300

    commit

--- DSM2/dw2-nodejs-express/js-aula01-tipos-de-funcoes/variaveis/script.js
+++ DSM2/dw2-nodejs-express/js-aula01-tipos-de-funcoes/variaveis/script.js
@@ -4,3 +4,7 @@
 //var: Evite o uso, pode nao ser seguro
 //let: utilizado quando for necessario reatribuir o valor da variavel
 //const: utilizar quando nao precisar reatribuir o valor da variavel
+
+var cidade = "Registro";
+var cidade = "Sete Barras";
+document.writeln(cidade);

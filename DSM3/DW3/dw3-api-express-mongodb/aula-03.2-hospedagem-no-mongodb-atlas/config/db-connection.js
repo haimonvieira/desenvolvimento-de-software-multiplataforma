0db332090fd99a80acc5d52eb51a5c1f606f99c3
Haimon Vieira <haimonvieira@pm.me>
2026-08-26 21:46:05 -0300

    Atualizado .env para .env convencional

--- DSM3/DW3/dw3-api-express-mongodb/aula-03.2-hospedagem-no-mongodb-atlas/config/db-connection.js
+++ DSM3/DW3/dw3-api-express-mongodb/aula-03.2-hospedagem-no-mongodb-atlas/config/db-connection.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
-import 'dotenv/config' // Carrega as variáveis do arquivo .env
+import dotenv from 'dotenv' // Carrega as variáveis do arquivo .env
+dotenv.config({
+    path: ['.env', 'atlas-credentials.env']
+});
 import dns from 'node:dns';
 dns.setServers(['8.8.8.8', '8.8.4.4']); // Força o uso do DNS do Google
 
 
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
+const dbUser = process.env.MONGODB_USERNAME;
+const dbPassword = process.env.MONGODB_PASSWORD;
 
 const connect = () => {
 
